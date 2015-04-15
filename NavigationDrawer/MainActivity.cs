134f0307aaf8cd58ede7f1aee1f2d8421f1215a7
bule82 <bulegato@gmail.com>
2015-04-15 10:23:43 +0200

    modify from web 1

--- NavigationDrawer/MainActivity.cs
+++ NavigationDrawer/MainActivity.cs
@@ -19,7 +19,7 @@ namespace NavigationDrawer
 		internal Sample[] mSamples;
 		internal GridView mGridView;
         internal String infos = "version";
-        internal String branch = "branch1-modify-web";
+        internal String branch = "branch1-modify-web1";
 
 		protected override void OnCreate (Bundle savedInstanceState)
 		{
